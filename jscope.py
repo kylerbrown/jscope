6ff5ca37012ada39948e86aea2eeefb76d15121d
Kyler Brown <kjbrown@pequod>
2013-07-03 19:07:24 -0500

    attempted pyqtgraph solution, but suffers from memory leak and not that much faster

--- jscope.py
+++ jscope.py
@@ -1,8 +1,9 @@
 import numpy as np
 import jack
 import sys
+import signal
 # Python Qt4 bindings for GUI objects
-from PySide import QtGui
+from PySide import QtGui, QtCore
 import matplotlib
 matplotlib.use('Qt4Agg')
 # Matplotlib Figure object
@@ -12,6 +13,10 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg \
 import FigureCanvasQTAgg as FigureCanvas
 import argparse
+
+PG = False
+if PG == True:
+    import pyqtgraph as pg
 
 class RingBuffer():
     def __init__(self, length):
@@ -27,6 +32,78 @@ class RingBuffer():
         idx = (self.index + np.arange(self.data.size)) % self.data.size
         return self.data[idx]
 
+class JScopePG(QtGui.QMainWindow):
+    def __init__(self, nscopes, subsamp_factor, length, zoom):
+        super(JScopePG, self).__init__()
+        self.scopes = range(nscopes)
+        self.subsamp_factor = subsamp_factor
+        self.plotlength = length
+        self.zoom = zoom
+        self.initJACK()
+        self.initUI()
+        self.timerEvent(None)
+        self.timer = self.startTimer(10)
+
+    def initUI(self):
+        self.data_layout = pg.GraphicsLayoutWidget()
+        #self.area = pgd.DockArea()
+        #data_dock = pgd.Dock("Data", size=(500, 200))
+        #self.area.addDock(data_dock, 'right')
+        #data_dock.addWidget(self.data_layout)
+        self.setCentralWidget(self.data_layout)
+
+        self.abscissa = -np.arange(self.Sr*self.plotlength) / self.Sr
+        self.input_slice = np.zeros((len(self.scopes), self.N), dtype='f')
+        self.output_slice = self.input_slice.copy()
+        self.pl = []
+        self.curve = []
+        self.input_ring = []
+        self.output_ring = []
+        for i in self.scopes:
+            self.pl.append(self.data_layout.addPlot(title=str(i)))
+            self.curve.append(self.pl[i].plot(pen='w'))
+            self.pl[i].showGrid(x=True, y=True)
+            self.pl[i].enableAutoRange('xy', False)
+            self.data_layout.nextRow()
+            #data_dock.addWidget(self.pl[i])
+            self.input_ring.append(RingBuffer(self.abscissa.size))
+            self.output_ring.append(RingBuffer(self.abscissa.size))
+        self.show()
+
+    def timerEvent(self, evt):
+        x = self.ReadFromJack()
+        for i in self.scopes:
+            self.input_ring[i].add(np.squeeze(x[i,:]))
+        self.plot()
+
+    def initJACK(self):
+        jack.attach("jscope")
+        for i in self.scopes:
+            jack.register_port("in_%d" %(i), jack.IsInput)
+        jack.activate()
+        self.N = jack.get_buffer_size()
+        self.Sr = float(jack.get_sample_rate())
+
+    def ReadFromJack(self):
+        try:
+            jack.process(self.input_slice, self.output_slice)
+        except jack.InputSyncError:
+            print "Input Sync Error"
+            pass
+        except jack.OutputSyncError:
+            print "Output Sync Error"
+            pass
+        return self.output_slice
+
+    def plot(self):
+        for i in self.scopes:
+            x = self.input_ring[i].data
+            #self.pl[i].clear()
+            #self.pl[i].plot(self.abscissa[::self.subsamp_factor],
+            #                x[::self.subsamp_factor])
+            self.curve[i].setData(self.abscissa, x)
+            #print(x)
+        #self.show()
 
 class JScopeWin(FigureCanvas):
     def __init__(self, nscopes, subsamp_factor, length, zoom):
@@ -39,11 +116,10 @@ class JScopeWin(FigureCanvas):
         print jack.get_ports()
         #jack.connect(jack.get_ports()[-2], "jscope:in_1")
 
-        # CONSTANTS
         self.N = jack.get_buffer_size()
         self.Sr = float(jack.get_sample_rate())
         self.plotlength = length #  plot length in seconds
-        self.abscissa = -np.arange(self.Sr*self.plotlength) / self.Sr
+        self.abscissa = np.flipud(-np.arange(self.Sr*self.plotlength) / self.Sr)
         
         self.input_slice = np.zeros((nscopes, self.N), dtype='f')
         self.output_slice = self.input_slice.copy()
@@ -103,6 +179,11 @@ class JScopeWin(FigureCanvas):
                                       x[::self.subsamp_factor])
         self.fig.canvas.draw()
 
+def sigint_handler(*args):
+    """Handler for the SIGINT signal."""
+    sys.stderr.write('\r')
+    QtGui.QApplication.quit()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Simple plotting tool using pyjack \
     and matplotlib')
@@ -119,16 +200,34 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # create the GUI application
-    app = QtGui.QApplication(sys.argv)
-    # Create our Matplotlib widget
-    widget = JScopeWin(args.nscopes, args.subsample, args.length, args.zoom)
-    # set the window title
-    widget.setWindowTitle("JScope")
-    # show the widget
-    widget.show()
+    if PG == True:
+        signal.signal(signal.SIGINT, sigint_handler)
+        app = QtGui.QApplication(sys.argv)
+        app.setApplicationName('jscope')
+        timer = QtCore.QTimer()
+        timer.start(500)  # You may change this if you wish.
+        timer.timeout.connect(lambda: None)  # Let the interpreter run each 500 ms.
+        mainWin = JScopePG(args.nscopes, args.subsample, args.length, args.zoom)
+    else:
+        app = QtGui.QApplication(sys.argv)
+        # Create our Matplotlib widget
+        widget = JScopeWin(args.nscopes, args.subsample, args.length, args.zoom)
+        # set the window title
+        #widget.setWindowTitle("JScope")
+        # show the widget
+        widget.show()
     # start the Qt main loop execution, exiting from this script
     # with the same return code of Qt application
     sys.exit(app.exec_())
-    jack.deactivate()
+    jack.deactivate() #todo add these one quit
     jack.detach()
+    print('done') #never prints
+
+
+
+
+
+
+
+
 
