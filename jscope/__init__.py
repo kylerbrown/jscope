556706b5399072e80e2faf3a03fd8f3cc3599bb9
Kyler Brown <kjbrown@pequod>
2013-09-21 17:04:21 -0500

    code organizing, added reset view button

---
+++ jscope/__init__.py
@@ -0,0 +1,6 @@
+'''
+jscope is a scrolling osciloscope for the JACK audio framework
+'''
+
+__all__ = ['jscope']
+__version__ = '0.1'
--- jscope/jscope.py
+++ jscope/jscope.py
@@ -1,50 +1,62 @@
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
-from pyqtgraph.widgets import RemoteGraphicsView
 import numpy as np
 import jack
+
 
 class RingBuffer():
     "A 1D ring buffer using numpy arrays"
     def __init__(self, length):
         self.data = np.zeros(length, dtype='f')
         self.index = 0
+        self.fifo_indexes = np.arange(length)
 
-    def add(self, x):
+    def extend(self, x):
         "adds array x to ring buffer"
         x_index = (self.index + np.arange(x.size)) % self.data.size
         self.data[x_index] = x
         self.index = x_index[-1] + 1
 
-    def fifo(self):
+
+    def get(self):
         "Returns the first-in-first-out data in the ring buffer"
         idx = (self.index + np.arange(self.data.size)) % self.data.size
         return self.data[idx]
 
-
-def ReadFromJack(input_slice, output_slice):
-        try:
-            jack.process(input_slice, output_slice)
-        except jack.InputSyncError:
-            print "Input Sync Error"
+      
+def ReadFromJack(linput_slice=None, loutput_slice=None):
+    if linput_slice == None and loutput_slice == None:
+        global input_slice, output_slice
+    else:
+        input_slice = linput_slice
+        output_slice = loutput_slice
+    try:
+        jack.process(input_slice, output_slice)
+    except jack.InputSyncError:
+        print "Input Sync Error"
             #self.update_time += 20
             #print("setting update time to %d" %(self.update_time))
-            pass
-        except jack.OutputSyncError:
-            print "Output Sync Error"
-            pass
+        pass
+    except jack.OutputSyncError:
+        print "Output Sync Error"
+        pass
 
+def reset_view():
+    [p.setYRange(-1, 1) for p in plots]
+    [p.enableAutoRange(axis=pg.ViewBox.XAxis, enable=True)]
 
 # initialize jack connections
-nscopes = 2
+nscopes = 1
 jack.attach('jscope')
 for i in range(nscopes):
     jack.register_port("in_%d" %(i), jack.IsInput)
 jack.activate()
 N = jack.get_buffer_size()
 Sr = float(jack.get_sample_rate())
-plotlength = 1
+print(Sr)
+plotlength = .3
 abscissa = np.flipud(-np.arange(Sr*plotlength) / Sr)
+print(len(abscissa))
 resample_factor = int(abscissa.size / 1000)
 
 rings = [RingBuffer(len(abscissa)) for i in range(nscopes)]
@@ -54,16 +66,19 @@ output_slice = input_slice.copy()
 
 
 app = pg.mkQApp()
-#view = RemoteGraphicsView.RemoteGraphicsView()
-pg.setConfigOptions(antialias=True)
-#view.setWindowTitle('pyqtgraph example: RemoteSpeedTest')
 plots = [pg.PlotWidget() for i in range(nscopes)]
 lplt = pg.PlotWidget()
 label = QtGui.QLabel()
+reset_view_button = QtGui.QPushButton("Reset View")
+reset_view_button.clicked.connect(reset_view)
 layout = pg.LayoutWidget()
 layout.addWidget(label)
+layout.addWidget(reset_view_button)
 for i, p in enumerate(plots):
     p.showGrid(x=True, y=True)
+    p.setYRange(-1, 1)
+    p.NoDrag=False
+
     layout.addWidget(p, row=i+1, col=0, colspan=3)
 layout.resize(800,800)
 layout.show()
@@ -76,22 +91,28 @@ layout.show()
 lastUpdate = pg.ptime.time()
 avgFps = 0.0
 
+
+
 def update():
-    global label, lastUpdate, avgFps, input_slice, output_slice, plots, rings
-    ReadFromJack(input_slice, output_slice)
+    global label, lastUpdate, avgFps
+    #ReadFromJack(input_slice, output_slice)
     for i, p in enumerate(plots):
         data = np.squeeze(output_slice[i,:])
-        rings[i].add(data)
-        p.plot(rings[i].fifo(), clear=True)
+        rings[i].extend(data)
+        p.plot(abscissa, rings[i].get(), clear=True)
     now = pg.ptime.time()
     fps = 1.0 / (now - lastUpdate)
     lastUpdate = now
     avgFps = avgFps * 0.8 + fps * 0.2
     label.setText("Generating %0.2f fps" % avgFps)
-        
+
+
+jacktimer =  QtCore.QTimer()
+jacktimer.timeout.connect(ReadFromJack)
+jacktimer.start(0)
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
-timer.start(0)
+timer.start(50)
 
 def jack_cleanup():
     jack.deactivate()
